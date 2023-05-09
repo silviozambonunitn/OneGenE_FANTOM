e2d7c7c0548ff18cc8c83242a0edcc60b70ca99f
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-09 12:15:51 +0200

    Created the script

--- pearsonCorrelation/pearsonScript.R
+++ pearsonCorrelation/pearsonScript.R
@@ -1,4 +1,10 @@
-setwd(here::here())
-library(data.table)
-samples = fread(file='pearsonCorrelation/hgnc_data_mat.csv', sep=',', header = TRUE, data.table=F)
-matrix=cor(samples)
+library(readxl)
+samples = fread(file="~/Documents/OneGenE_FANTOM/pearsonCorrelation/hgnc_data_mat.csv", sep=',', header = TRUE, data.table=F)
+samples[2:nrow(samples),]
+rownames(samples) <- samples$V1
+samples <- samples[,-1]
+res <- cor(samples)
+write.csv(res, file="~/Documents/OneGenE_FANTOM/pearsonCorrelation/FANTOM_OneGenE_PearsonMatrix.csv", eol = '\n', quote = FALSE)
+#which(res == min(res), arr.ind = TRUE)
+median(res)
+mean(res)
