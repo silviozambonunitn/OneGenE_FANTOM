3830e543cff4ecf72408263c21bf3558b0b7795d
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-10 11:28:56 +0200

    implemented exist function

--- pearsonCorrelation/pearsonScript.cpp
+++ pearsonCorrelation/pearsonScript.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+bool exists(map<pair<string,string>,float>* m, pair<string,string>* p){
+    pair<string,string> reverse;
+    reverse.first=p->second;
+    reverse.second=p->first;
+    return m->count(*p)!=0 || m->count(reverse)!=0;
+}
+
 int main(int argc,char* argv[]){
     ifstream in("hgnc_data_mat.csv");
     map<pair<string,string>,float> matrix; //Usare set invece di int per evitare duplicati? Il set è ordinato
