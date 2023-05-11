64a0b676fe02706a3608a7dd781b77aec750887e
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-11 12:02:11 +0200

    Added statistics

--- pearsonCorrelation/pearsonScript.R
+++ pearsonCorrelation/pearsonScript.R
@@ -7,6 +7,5 @@ samples <- samples[,-1] #Deleting the first column
 rotated=t(samples) #To rotate the matrix
 res = cor(rotated, method="pearson")
 write.csv(res, file="~/Documents/OneGenE_FANTOM/pearsonCorrelation/FANTOM_OneGenE_PearsonMatrix.csv", eol = '\n', quote = FALSE) #Change if not running on the rdds pc
-#which(res == min(res), arr.ind = TRUE)
-median(res)
-mean(res)
+s=summary(res)
+write.csv(res, file="~/Documents/OneGenE_FANTOM/pearsonCorrelation/FANTOM_OneGenE_PearsonMatrix_Summary.csv", eol = '\n', quote = FALSE)
