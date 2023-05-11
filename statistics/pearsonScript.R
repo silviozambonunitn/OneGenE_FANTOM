6293bff5056e846dac03e0cc2d1456282481b5cb
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-11 14:33:30 +0200

    moved script

--- statistics/pearsonScript.R
+++ statistics/pearsonScript.R
@@ -6,6 +6,6 @@ rownames(samples) <- samples$V1 #Setting the rownames from the first column
 samples <- samples[,-1] #Deleting the first column
 rotated=t(samples) #To rotate the matrix
 res = cor(rotated, method="pearson")
-write.csv(res, file="/storage/shared/fantom/FANTOM_OneGenE_PearsonMatrix.csv", eol = '\n', quote = FALSE)
+write.csv(res, file="/storage/shared/fantom/FANTOM_OneGenE_PearsonMatrix.csv", eol='\n', quote = FALSE)
 s=summary(res)
 write.csv(res, file="/storage/shared/fantom/FANTOM_OneGenE_PearsonMatrix_Summary.csv", eol = '\n', quote = FALSE)
