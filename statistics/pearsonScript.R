f3d4493ae8f607f28f3aa731ab0fa4b7ab7e4b01
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-18 16:34:14 +0200

    Added plot

--- statistics/pearsonScript.R
+++ statistics/pearsonScript.R
@@ -1,13 +1,13 @@
 library(readxl)
 library(data.table)
 samples = fread(
-  file = "/home/silvio/Documents/OneGenE_FANTOM/hgnc_data_mat.csv",
+  file = "/storage/shared/fantom/hgnc_data_mat.csv",
   sep = ',',
   header = TRUE,
   data.table = F
 )
 rownames(samples) = samples$V1 #Setting the rownames from the first column
-samples = samples[,-1] #Deleting the first column
+samples = samples[, -1] #Deleting the first column
 rotated = t(samples) #To rotate the matrix, might delete now if not for efficiency
 
 output = data.frame()
@@ -15,8 +15,8 @@ output = data.frame()
 n = 1
 for (a in 1:nrow(rotated)) {
   for (b in 1:nrow(rotated)) {
+    #check if already calculated
     if (TRUE) {
-      #check if already calculated
       res = cor(rotated[, a], rotated[, b])
       output[n, 1] = colnames(rotated)[a]
       output[n, 2] = colnames(rotated)[b]
@@ -30,6 +30,17 @@ write.csv(output,
           file = "/storage/shared/fantom/FANTOM_PearsonMatrix.csv",
           eol = '\n',
           quote = FALSE)
+
+library(ggplot2)
+p = ggplot(data = output[, 3]) + geom_density() + geom_vline(aes(xintercept = mean(output[, 3])),
+                                                             color = "blue",
+                                                             size = 1)
+ggsave(
+  filename = "corrDistr.jpeg",
+  path = "/storage/shared/fantom/",
+  plot = p,
+  device = "svg"
+)
 
 #s=summary(res) #trash
 #write.csv(res, file="/storage/shared/fantom/FANTOM_PM_Summary.csv", eol = '\n', quote = FALSE)
