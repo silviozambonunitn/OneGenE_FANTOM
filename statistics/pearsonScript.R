3fd1794b3dcb6852806616d5f52b08165629682f
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-18 15:30:50 +0200

    Changed the matrix output

--- statistics/pearsonScript.R
+++ statistics/pearsonScript.R
@@ -1,17 +1,32 @@
 library(readxl)
 library(data.table)
 samples = fread(
-  file = "/storage/shared/fantom/hgnc_data_mat.csv",
+  file = "/home/silvio/Documents/OneGenE_FANTOM/hgnc_data_mat.csv",
   sep = ',',
   header = TRUE,
   data.table = F
 )
-samples[2:nrow(samples), ]
 rownames(samples) = samples$V1 #Setting the rownames from the first column
-samples = samples[, -1] #Deleting the first column
-rotated = t(samples) #To rotate the matrix
-res = cor(rotated, method = "pearson")
-write.csv(res,
+samples = samples[,-1] #Deleting the first column
+rotated = t(samples) #To rotate the matrix, might delete now if not for efficiency
+
+output = data.frame()
+
+n = 1
+for (a in 1:nrow(rotated)) {
+  for (b in 1:nrow(rotated)) {
+    if (TRUE) {
+      #check if already calculated
+      res = cor(rotated[, a], rotated[, b])
+      output[n, 1] = colnames(rotated)[a]
+      output[n, 2] = colnames(rotated)[b]
+      output[n, 3] = res
+      n = n + 1
+    }
+  }
+}
+
+write.csv(output,
           file = "/storage/shared/fantom/FANTOM_PearsonMatrix.csv",
           eol = '\n',
           quote = FALSE)
