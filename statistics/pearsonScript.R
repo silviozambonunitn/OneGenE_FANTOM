437fe558bbd52c932fe172d7c517a83541bea95c
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-22 10:29:58 +0200

    udated output

--- statistics/pearsonScript.R
+++ statistics/pearsonScript.R
@@ -1,22 +1,22 @@
 library(readxl)
 library(data.table)
 samples = fread(
-  file = "/storage/shared/fantom/hgnc_data_mat.csv",
+  file = "/home/silvio/Documents/OneGenE_FANTOM/hgnc_data_mat.csv",
   sep = ',',
   header = TRUE,
   data.table = F
 )
 rownames(samples) = samples$V1 #Setting the rownames from the first column
-samples = samples[, -1] #Deleting the first column
-rotated = t(samples) #To rotate the matrix, might delete now if not for efficiency
+samples = samples[,-1] #Deleting the first column
+rotated = t(samples) #To rotate the matrix, might delete now if not for efficiency of iteration
 
 output = data.frame()
 
 n = 1
 for (a in 1:nrow(rotated)) {
   for (b in 1:nrow(rotated)) {
-    #check if already calculated
-    if (TRUE) {
+    #must check if already calculated
+    if (!(colnames(rotated)[a] == colnames(rotated)[b])) {
       res = cor(rotated[, a], rotated[, b])
       output[n, 1] = colnames(rotated)[a]
       output[n, 2] = colnames(rotated)[b]
@@ -26,21 +26,16 @@ for (a in 1:nrow(rotated)) {
   }
 }
 
-write.csv(output,
-          file = "/storage/shared/fantom/FANTOM_PearsonMatrix.csv",
-          eol = '\n',
-          quote = FALSE)
-
-library(ggplot2)
-p = ggplot(data = output[, 3]) + geom_density() + geom_vline(aes(xintercept = mean(output[, 3])),
-                                                             color = "blue",
-                                                             size = 1)
-ggsave(
-  filename = "corrDistr.jpeg",
-  path = "/storage/shared/fantom/",
-  plot = p,
-  device = "svg"
+fwrite(
+  output,
+  file = "/home/silvio/Documents/OneGenE_FANTOM/FANTOM_PearsonMatrix.csv",
+  eol = '\n',
+  quote = FALSE,
+  row.names = FALSE,
+  col.names = FALSE
 )
 
-#s=summary(res) #trash
-#write.csv(res, file="/storage/shared/fantom/FANTOM_PM_Summary.csv", eol = '\n', quote = FALSE)
+library(ggplot2)
+ggplot() + geom_density(aes(output[, 3])) + geom_vline(aes(xintercept = mean(output[, 3])),
+                                                       color = "blue",
+                                                       size = 1)
