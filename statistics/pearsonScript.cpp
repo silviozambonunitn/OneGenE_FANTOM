9ce7d2888003f50973b50e544a0d9b48ee04622d
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-23 16:58:24 +0200

    Now has colnames

--- statistics/pearsonScript.cpp
+++ statistics/pearsonScript.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -11,16 +12,25 @@ int main(int argc, char* argv[]) {
     auto start = chrono::high_resolution_clock::now();
     unordered_map<pair<string, string>, float, boost::hash<pair<string, string>>> corMatrix;
 
+    //Getting all tids names
+    vector<string> rownames;
+    string buffer;
+    ifstream in("/home/silvio/Documents/OneGenE_FANTOM/hgnc_data_mat.csv");
+    getline(in, buffer);  // skipping header
+    while (getline(in, buffer, ',')) {
+        rownames.push_back(buffer);
+        getline(in, buffer);  // skip rest of the row
+    }
+
     string t1, t2;
     arma::mat samples;
     arma::field<string> header;
     samples.load(arma::csv_name("/home/silvio/Documents/OneGenE_FANTOM/hgnc_data_mat.csv", header, arma::csv_opts::trans));  // Loads transposed matrix
-    // auto corMatrix = arma::cor(samples);
-    // corMatrix.brief_print();
+
     for (int i = 0; i < samples.n_cols; i++) {
         for (int j = 0; j < samples.n_cols; j++) {
-            if (i != j && (corMatrix.find(minmax(t1, t2)) == corMatrix.end())) {  // Not the same isoform and not yet calculated
-                //Calcola corr e inserisci in hashmap
+            if (i != j && (corMatrix.find(minmax(rownames.at(i), rownames.at(j))) == corMatrix.end())) {  // Not the same isoform and not yet calculated
+                corMatrix[minmax(t1, t2)] = arma::as_scalar(arma::cor(samples.col(i), samples.col(j)));
             }
         }
     }
