77e2373c1352b1cf6f3e8e4c25c9d9366e41c875
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-24 15:43:54 +0200

    tried fixing save file

--- statistics/frequenciesHistogram.R
+++ statistics/frequenciesHistogram.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(data.table)
+library(Cairo)
 
 #opening and parsing csv matrix file
 setwd(here::here())
@@ -8,9 +9,12 @@ frequencies = fread(
   sep = ';',
 )
 
-plot=ggplot(frequencies, aes(RelativeFrequency)) +
+plot1=ggplot(frequencies, aes(RelativeFrequency)) +
   scale_x_continuous(breaks = seq(0, 1, 0.05)) +
   geom_histogram(bins = 50) +
   theme_bw()
 
-ggsave("histogram_plot.png", plot, width = 7, height = 5, units = "in", dpi = 300)
+#ggsave("histogram_plot.png", plot1, width = 7, height = 5, units = "in", dpi = 300, device = "png")
+CairoPNG("histogram_plot.png", width = 7, height = 5, units = "in", dpi = 300)
+print(plot)
+dev.off()
