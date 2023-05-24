1b72f04e69e6badc97ccef1485d7662115f560ec
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-24 17:01:32 +0200

    updated plotting

--- statistics/frequenciesHistogram.R
+++ statistics/frequenciesHistogram.R
@@ -1,20 +1,20 @@
 library(ggplot2)
 library(data.table)
-library(Cairo)
 
 #opening and parsing csv matrix file
 setwd(here::here())
 frequencies = fread(
-  file = '/storage/shared/fantom/FANTOM_RelativeFrequencyMatrix.csv',
-  sep = ';',
+  file = '/home/silvio/Documents/OneGenE_FANTOM/FANTOM_RelativeFrequencyMatrix.csv',
+  sep = ';'
 )
 
 plot1=ggplot(frequencies, aes(RelativeFrequency)) +
   scale_x_continuous(breaks = seq(0, 1, 0.05)) +
   geom_histogram(bins = 50) +
   theme_bw()
+ggsave("histogram_plot.png", plot1, width = 7, height = 5, units = "in", dpi = 300, device = "png")
+stats=frequencies[,summary(RelativeFrequency)]
 
-#ggsave("histogram_plot.png", plot1, width = 7, height = 5, units = "in", dpi = 300, device = "png")
-CairoPNG("histogram_plot.png", width = 7, height = 5, units = "in", dpi = 300)
-print(plot)
-dev.off()
+#CairoPNG("histogram_plot.png", width = 7, height = 5, units = "in", dpi = 300)
+#print(plot)
+#dev.off()
