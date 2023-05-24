c2e389e2abf8bcb6edc9a40441fb1868501b079f
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-24 12:53:19 +0200

    completely redone

--- statistics/frequenciesHistogram.R
+++ statistics/frequenciesHistogram.R
@@ -1,9 +1,17 @@
+library(ggplot2)
+library(data.table)
+library(readxl)
+
 #opening and parsing csv matrix file
 setwd(here::here())
-frequencies = read.csv(file='/storage/shared/fantom/FANTOM_RelativeFrequencyMatrix.csv', sep=';', colClasses = c("character", "character", "numeric"))
+frequencies = fread(
+  file = '/storage/shared/fantom/FANTOM_RelativeFrequencyMatrix.csv',
+  sep = ';',
+)
 
-library(ggplot2)
-ggplot(frequencies, aes(RelativeFrequency))+
-  scale_x_continuous(breaks=seq(0, 1, 0.05))+
-  geom_histogram(bins = 50)+
+plot=ggplot(frequencies, aes(RelativeFrequency)) +
+  scale_x_continuous(breaks = seq(0, 1, 0.05)) +
+  geom_histogram(bins = 50) +
   theme_bw()
+
+ggsave("histogram_plot.png", plot, width = 7, height = 5, units = "in", dpi = 300)
