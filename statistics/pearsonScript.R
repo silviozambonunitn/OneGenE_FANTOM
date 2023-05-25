506c1bec8c01e6f26ada30d41187937f01f8bfc6
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-25 15:04:34 +0200

    Reimplemented control logic

--- statistics/pearsonScript.R
+++ statistics/pearsonScript.R
@@ -1,37 +1,37 @@
-# samples = fread(
-#   file = "/home/silvio/Documents/OneGenE_FANTOM/hgnc_data_mat.csv",
-#   sep = ',',
-#   header = TRUE,
-#   data.table = F
-# )
-# rownames(samples) = samples$V1 #Setting the rownames from the first column
-# samples = samples[,-1] #Deleting the first column
-# rotated = t(samples) #To rotate the matrix, might delete now if not for efficiency of iteration
-#
-# output = data.frame()
-#
-# n = 1
-# for (a in 1:nrow(rotated)) {
-#   for (b in 1:nrow(rotated)) {
-#     #must check if already calculated
-#     if (!(colnames(rotated)[a] == colnames(rotated)[b])) {
-#       res = cor(rotated[, a], rotated[, b])
-#       output[n, 1] = colnames(rotated)[a]
-#       output[n, 2] = colnames(rotated)[b]
-#       output[n, 3] = res
-#       n = n + 1
-#     }
-#   }
-# }
-#
-# fwrite(
-#   output,
-#   file = "/home/silvio/Documents/OneGenE_FANTOM/FANTOM_PearsonMatrix.csv",
-#   eol = '\n',
-#   quote = FALSE,
-#   row.names = FALSE,
-#   col.names = FALSE
-# )
+samples = fread(
+  file = "/home/silvio/Documents/OneGenE_FANTOM/hgnc_data_mat.csv",
+  sep = ',',
+  header = TRUE,
+  data.table = F
+)
+rownames(samples) = samples$V1 #Setting the rownames from the first column
+samples = samples[,-1] #Deleting the first column
+rotated = t(samples) #To rotate the matrix, might delete now if not for efficiency of iteration
+
+output = data.frame()
+
+n = 1
+for (a in 1:nrow(rotated)) {
+  for (b in 1:nrow(rotated)) {
+    #must check if already calculated
+    if (!(colnames(rotated)[a] == colnames(rotated)[b])) {
+      res = cor(rotated[, a], rotated[, b])
+      output[n, 1] = colnames(rotated)[a]
+      output[n, 2] = colnames(rotated)[b]
+      output[n, 3] = res
+      n = n + 1
+    }
+  }
+}
+
+fwrite(
+  output,
+  file = "/home/silvio/Documents/OneGenE_FANTOM/FANTOM_PearsonMatrix2.csv",
+  eol = '\n',
+  quote = FALSE,
+  row.names = FALSE,
+  col.names = FALSE
+)
 
 library(data.table)
 matrix = fread(
