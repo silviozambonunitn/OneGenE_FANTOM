f8eda7751e5d421a3618d97e89d6dd98778be1fb
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-26 11:51:17 +0200

    fixed

--- statistics/pearsonScript.R
+++ statistics/pearsonScript.R
@@ -6,10 +6,9 @@ samples = fread(
   data.table = F
 )
 rownames(samples) = samples$V1 #Setting the rownames from the first column
-samples = samples[, -1] #Deleting the first column
+samples = samples[,-1] #Deleting the first column
 rotated = t(samples) #To rotate the matrix, might delete now if not for efficiency of iteration
 rm(samples)
-
 library(HiClimR)
 output = fastCor(
   rotated,
@@ -18,43 +17,24 @@ output = fastCor(
   verbose = TRUE,
   nSplit = 1
 )
+csvout = na.omit(as.data.frame(as.table(output)))
 
-#output = data.frame()
-#
-# n = 1
-# for (a in 1:nrow(rotated)) {
-#   for (b in 1:nrow(rotated)) {
-#     #must check if already calculated
-#     if (!(colnames(rotated)[a] == colnames(rotated)[b])) {
-#       res = cor(rotated[, a], rotated[, b])
-#       output[n, 1] = colnames(rotated)[a]
-#       output[n, 2] = colnames(rotated)[b]
-#       output[n, 3] = res
-#       n = n + 1
-#     }
-#   }
-# }
-#
 fwrite(
-  output,
-  file = "/home/silvio/Documents/OneGenE_FANTOM/FANTOM_PearsonMatrix2.csv",
+  csvout,
+  file = "/storage/shared/fantom/FANTOM_PearsonMatrix2.csv",
   eol = '\n',
   quote = FALSE,
   row.names = FALSE,
   col.names = FALSE
 )
-#
-# library(data.table)
-# matrix = fread(
-#   file = "/home/silvio/Documents/OneGenE_FANTOM/pearsonMatrix2.csv",
-#   sep = ',',
-#   header = FALSE
-# )
 
 library(ggplot2)
-ggplot() + geom_density(aes(matrix[, 3])) + geom_vline(aes(xintercept = mean(matrix[, 3])),
-                                                       color = "blue",
-                                                       size = 1)
+plot1 = ggplot() +
+  geom_density(aes(csvout[, 3])) +
+  geom_vline(aes(xintercept = mean(csvout[, 3])),
+             color = "blue",
+             size = 1)
+
 ggsave(
   path = "/storage/shared/fantom/",
   filename = "pearsonPlot.png",
@@ -65,4 +45,4 @@ ggsave(
   dpi = 300,
   device = "png"
 )
-stats = matrix[, summary(V3)] #change V3 to correct index
+stats = csvout[, summary(Freq)]
