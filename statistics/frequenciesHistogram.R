87f9c6f2098aa2bea9a56bddf4d33e4cd5cd4736
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-05-26 15:49:01 +0200

    updated

--- statistics/frequenciesHistogram.R
+++ statistics/frequenciesHistogram.R
@@ -3,18 +3,21 @@ library(data.table)
 
 #opening and parsing csv matrix file
 setwd(here::here())
-frequencies = fread(
-  file = '/home/silvio/Documents/OneGenE_FANTOM/FANTOM_RelativeFrequencyMatrix.csv',
-  sep = ';'
-)
+frequencies = fread(file = '/home/silvio/Documents/OneGenE_FANTOM/FANTOM_RelativeFrequencyMatrix.csv',
+                    sep = ';')
 
-plot1=ggplot(frequencies, aes(RelativeFrequency)) +
+print(summary(frequencies[, 3]))
+
+ggplot(frequencies, aes(RelativeFrequency)) +
   scale_x_continuous(breaks = seq(0, 1, 0.05)) +
   geom_histogram(bins = 50) +
   theme_bw()
-ggsave("histogram_plot.png", plot1, width = 7, height = 5, units = "in", dpi = 300, device = "png")
-stats=frequencies[,summary(RelativeFrequency)]
 
-#CairoPNG("histogram_plot.png", width = 7, height = 5, units = "in", dpi = 300)
-#print(plot)
-#dev.off()
+ggsave(
+  "histrogramRelFreq.png",
+  width = 7,
+  height = 5,
+  units = "in",
+  dpi = 300,
+  device = "png"
+)
--- statistics/pearsonScript.R
+++ statistics/pearsonScript.R
@@ -6,21 +6,24 @@ samples = fread(
   data.table = F
 )
 rownames(samples) = samples$V1 #Setting the rownames from the first column
-samples = samples[,-1] #Deleting the first column
+samples = samples[, -1] #Deleting the first column
 rotated = t(samples) #To rotate the matrix, might delete now if not for efficiency of iteration
 rm(samples)
+rotated = rotated[, 1:200]
 library(HiClimR)
-output = fastCor(
+matrix = fastCor(
   rotated,
   upperTri = TRUE,
   optBLAS = TRUE,
   verbose = TRUE,
-  nSplit = 1
+  nSplit = 10
 )
-csvout = na.omit(as.data.frame(as.table(output)))
+matrix = na.omit(as.data.frame(as.table(matrix)))
+
+print(summary(matrix[, 3]))
 
 fwrite(
-  csvout,
+  matrix,
   file = "/storage/shared/fantom/FANTOM_PearsonMatrix2.csv",
   eol = '\n',
   quote = FALSE,
@@ -29,20 +32,18 @@ fwrite(
 )
 
 library(ggplot2)
-plot1 = ggplot() +
-  geom_density(aes(csvout[, 3])) +
-  geom_vline(aes(xintercept = mean(csvout[, 3])),
-             color = "blue",
-             size = 1)
+ggplot() +
+  theme_bw() +
+  geom_density(aes(matrix[, 3])) +
+  geom_vline(aes(xintercept = mean(matrix[, 3])),
+             color = "blue") +
+  labs(x = "Pearson correlation coefficient", y = "Density", title = "Distribution", caption = "Blue line = mean")
 
 ggsave(
-  path = "/storage/shared/fantom/",
-  filename = "pearsonPlot.png",
-  plot1,
+  filename = "pearsonDistr.png",
   width = 7,
   height = 5,
   units = "in",
   dpi = 300,
   device = "png"
 )
-stats = csvout[, summary(Freq)]
