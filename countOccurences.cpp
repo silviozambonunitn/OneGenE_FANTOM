d3e4e07138a5b80affaac1f2bac8dd4a53f1f20c
silviozambonunitn <silvio.zambon@studenti.unitn.it>
2023-06-14 10:33:03 +0200

    Added debugging

--- countOccurences.cpp
+++ countOccurences.cpp
@@ -1,5 +1,6 @@
 /*
 This script counts the number of Pearson correlations greater than 0.046
+Compile using g++ -Wall -O3 -o countOcc.o countOccurences.cpp -larmadillo -llapack -lblas -fopenmp
 */
 
 #include <armadillo>
@@ -8,9 +9,16 @@ This script counts the number of Pearson correlations greater than 0.046
 int main() {
     arma::mat m;
     arma::field<std::string> header;
-    m.load(arma::csv_name("/storage/shared/FANTOM_PearsonMatrix_triangular.csv", header));
 
-    float threshold = 0.046;
+    std::cout << "Loading the matrix... " << std::flush;
+    const bool success = m.load(arma::csv_name("/storage/shared/FANTOM_PearsonMatrix_triangular.csv", header));
+    if (success) {
+        std::cout << "Done!\n";
+    } else {
+        std::cout << "Failed!\n";
+    }
+
+    double threshold = 0.046;
     arma::uvec indices = arma::find(m > threshold);
     std::size_t count = indices.n_elem;
 
